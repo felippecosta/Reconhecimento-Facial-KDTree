b1906d43f973e581f41e6f96200110e1d42ad11d
Felippe Costa <felippe_costa1@hotmail.com>
2025-06-08 15:27:50 -0400

    Delete utilitarios.py

--- utilitarios.py
+++
@@ -1,6 +0,0 @@
-#FELIPPE ALLAN COSTA
-import numpy as np
-
-# Função para calcular a distância Euclidiana entre dois vetores
-def distancia_euclidiana(v1, v2):
-    return np.linalg.norm(np.array(v1) - np.array(v2))
